41f325d3ce2090227157a4c10080da0ebccdb3fa
Shourya Singh Sengar <Shouryacode2021@gmail.com>
2023-10-13 05:07:49 +0530

    first commit

---
+++ src/Navigation/Nav.js
@@ -0,0 +1,26 @@
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+import "./Nav.css";
+
+const Nav = ({ handleInputChange, query }) => {
+  return (
+    <nav>
+
+      <div className="nav-container">
+        <input
+          className="search-input"
+          type="text"
+          onChange={handleInputChange}
+          value={query}
+          placeholder="Enter your search shoes."
+        />
+      </div>
+      <div className="profile-container">
+        <AiOutlineShoppingCart className="nav-icons" />
+        <AiOutlineUserAdd className="nav-icons" />
+
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
